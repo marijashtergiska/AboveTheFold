46e4561fa79b3f83912ca03eba074726757c60a0
marijashtergiska <94129078+marijashtergiska@users.noreply.github.com>
2024-01-29 13:58:46 +0100

    Add files via upload

---
+++ Skopje.js
@@ -0,0 +1,36 @@
+$( function() {
+    $( "#menu" ).menu();
+} );
+
+$( function() {
+    $( document ).tooltip();
+} );
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("changePhoto");
+    let dots = document.getElementsByClassName("photoNumber");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
